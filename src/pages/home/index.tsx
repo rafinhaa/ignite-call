59cc726d03ec6840864b69c1c0fba70c3bac46ec
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-12-26 15:06:07 -0300

    Cadastro de usuário - Estrutura visual da home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/appPreview.png'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          alt="Calendário simbolizando a aplicação em andamento"
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
