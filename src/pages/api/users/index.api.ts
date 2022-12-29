b295f9cb37cbe1ef4e9b6efd76aea033b31e109a
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-12-29 12:01:30 -0300

    Cadastro de usuário - Manipulando cookies no Next

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,8 @@
 import { prisma } from '../../../lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
+
+const SEVEN_DAYS = 60 * 60 * 24 * 7
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,11 +12,23 @@ export default async function handler(
 
   const { name, username } = req.body
 
+  const userExists = await prisma.user.findUnique({
+    where: { username },
+  })
+
+  if (userExists)
+    return res.status(400).json({ message: 'User already exists' })
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: SEVEN_DAYS,
+    path: '/',
   })
 
   return res.status(201).json(user)
