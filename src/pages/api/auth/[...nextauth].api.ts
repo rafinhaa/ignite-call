0cb03f0dbd948e9dbc9444e610cf4febfa7bfbfa
Rafael Soncine <rafael.soncine@meliuz.com.br>
2023-01-03 11:25:00 -0300

    Conexão com o calendário - Permissão de acesso ao calendário

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,13 +1,29 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+const USER_INFO_EMAIL = 'https://www.googleapis.com/auth/userinfo.email'
+const USER_INFO_PROFILE = 'https://www.googleapis.com/auth/userinfo.profile'
+const CALENDAR = 'https://www.googleapis.com/auth/calendar'
+
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope: `${USER_INFO_EMAIL} ${USER_INFO_PROFILE} ${CALENDAR}`,
+        },
+      },
     }),
   ],
+  callbacks: {
+    async signIn({ account }) {
+      if (!account?.scope?.includes(CALENDAR))
+        return '/register/connect-calendar/?error=permissions'
+      return true
+    },
+  },
 }
 
 export default NextAuth(authOptions)
