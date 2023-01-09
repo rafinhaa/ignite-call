f9dd4633b7e6284d9b7fd9a4d82b5c7aea89774a
Rafael Soncine <rafael.soncine@meliuz.com.br>
2023-01-09 11:23:56 -0300

    Configurando disponibilidade - Validação e transformação no Zod

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -18,10 +18,29 @@ import {
   IntervalInputs,
   IntervalItem,
   IntervalContainer,
+  FormError,
 } from './styles'
 import { getWeekDays } from '../../../utils/get-week-days'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-const timeInteravalsFormSchema = z.object({})
+const timeIntervalsFormSchema = z.object({
+  intervals: z
+    .array(
+      z.object({
+        weekDay: z.number().min(0).max(6),
+        enabled: z.boolean(),
+        startTime: z.string(),
+        endTime: z.string(),
+      }),
+    )
+    .length(7)
+    .transform((intervals) => intervals.filter((interval) => interval.enabled))
+    .refine((intervals) => intervals.length > 0, {
+      message: 'Você precisa selecionar pelo menos um dia da semana!',
+    }),
+})
+
+type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
 
 const TimeIntervals = () => {
   const {
@@ -31,6 +50,7 @@ const TimeIntervals = () => {
     watch,
     formState: { isSubmitting, errors },
   } = useForm({
+    resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
         { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
@@ -52,7 +72,7 @@ const TimeIntervals = () => {
   const weekDays = getWeekDays()
   const intervals = watch('intervals')
 
-  const handleSetTimeIntervals = async (data) => {}
+  const handleSetTimeIntervals = async (data: TimeIntervalsFormData) => {}
 
   return (
     <Container>
@@ -104,8 +124,10 @@ const TimeIntervals = () => {
             </IntervalItem>
           ))}
         </IntervalContainer>
-
-        <Button type="submit">
+        {errors.intervals && (
+          <FormError size="sm">{errors.intervals.message}</FormError>
+        )}
+        <Button type="submit" disabled={isSubmitting}>
           Próximo passo
           <ArrowRight />
         </Button>
