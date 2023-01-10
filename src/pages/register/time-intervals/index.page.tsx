9b390e1e5159fa3c7a1cc0b6c78b22ddabfb5692
Rafael Soncine <rafael.soncine@meliuz.com.br>
2023-01-10 10:17:14 -0300

    Configurando disponibilidade - Input e output do Zod

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -22,6 +22,7 @@ import {
 } from './styles'
 import { getWeekDays } from '../../../utils/get-week-days'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { convertTimeStringToMinutes } from '../../../utils/convert-time-string-to-minutes'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -37,10 +38,32 @@ const timeIntervalsFormSchema = z.object({
     .transform((intervals) => intervals.filter((interval) => interval.enabled))
     .refine((intervals) => intervals.length > 0, {
       message: 'Você precisa selecionar pelo menos um dia da semana!',
-    }),
+    })
+    .transform((intervals) =>
+      intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        }
+      }),
+    )
+    .refine(
+      (intervals) => {
+        return intervals.every(
+          (interval) =>
+            interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes,
+        )
+      },
+      {
+        message:
+          'O horário do termino deve ter pelo menos 1 hora a mais do horário de início!',
+      },
+    ),
 })
 
-type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>
 
 const TimeIntervals = () => {
   const {
@@ -49,7 +72,7 @@ const TimeIntervals = () => {
     control,
     watch,
     formState: { isSubmitting, errors },
-  } = useForm({
+  } = useForm<TimeIntervalsFormInput>({
     resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
@@ -72,7 +95,12 @@ const TimeIntervals = () => {
   const weekDays = getWeekDays()
   const intervals = watch('intervals')
 
-  const handleSetTimeIntervals = async (data: TimeIntervalsFormData) => {}
+  const handleSetTimeIntervals = async (
+    data: any | TimeIntervalsFormOutput,
+  ) => {
+    const { intervals } = data
+    console.log(intervals)
+  }
 
   return (
     <Container>
