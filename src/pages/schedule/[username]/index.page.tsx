17eb57a75ed96fee96f7a070386125e291664d71
Rafael Soncine <rafael.soncine@meliuz.com.br>
2023-01-15 11:08:05 -0300

    Calendário e disponibilidade - Página de agendamento

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,55 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '../../../lib/prisma'
+import { Container, UserHeader } from './styles'
+
+interface ScheduleProps {
+  user: {
+    name: string,
+    bio: string,
+    avatarUrl: string,
+  };
+}
+
+const Schedule = ({ user: { name, bio, avatarUrl } }: ScheduleProps) => {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={avatarUrl} />
+        <Heading>{name}</Heading>
+        <Text>{bio}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+export default Schedule
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) return { notFound: true }
+
+  return {
+    props: {
+      user: {
+        name: user.username,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24,
+  }
+}
