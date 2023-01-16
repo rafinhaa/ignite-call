7dc3b3d317448d869eba0484e493bdb1bb003cb9
Rafael Soncine <rafael.soncine@meliuz.com.br>
2023-01-16 11:20:59 -0300

    Calendário e disponibilidade - Estrutura visual do calendário

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -1,6 +1,7 @@
 import { Avatar, Heading, Text } from '@ignite-ui/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { prisma } from '../../../lib/prisma'
+import { ScheduleForm } from './ScheduleForm'
 import { Container, UserHeader } from './styles'
 
 interface ScheduleProps {
@@ -19,6 +20,8 @@ const Schedule = ({ user: { name, bio, avatarUrl } }: ScheduleProps) => {
         <Heading>{name}</Heading>
         <Text>{bio}</Text>
       </UserHeader>
+
+      <ScheduleForm />
     </Container>
   )
 }
--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,11 +1,17 @@
-export const getWeekDays = () => {
+interface GetWeekDays {
+  short?: boolean;
+}
+
+export const getWeekDays = ({ short }: GetWeekDays) => {
   const formatter = new Intl.DateTimeFormat('pt-BR', {
     weekday: 'long',
   })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
-    .map((weekDay) =>
-      weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1)),
-    )
+    .map((weekDay) => {
+      return short
+        ? weekDay.slice(0, 3).toLocaleUpperCase()
+        : weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
+    })
 }
